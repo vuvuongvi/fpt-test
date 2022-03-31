298e76ca700a3e059252303c412d09547c57013f
Vi Vu <vivu@kms-technology.com>
2022-03-31 13:20:55 +0700

    feat: init and add algo calculate prime number

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [fName, setfName] = useState('');
+  const [fNumber, setNumber] = useState('');
+  const submitValue = () => {
+    const frmdetails = {
+      fName
+    }
+    let calculatePrimeValue = prime(frmdetails.fName)
+    setNumber(calculatePrimeValue);
+}
+  const prime = (number) => {
+    if (number && 1) {
+      number -= 2;
+    } else {
+      number--;
+    }
+    let i, j;
+    for (i = number; i >= 2; i -= 2) {
+      if (i % 2 === 0) {
+        continue;
+      }
+      for (j = 3; j <= Math.sqrt(i); j += 2) {
+        if (i % j == 0)
+          break;
+      }
+      if (j > Math.sqrt(i))
+        return i;
+    }
+    return 2;
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="number" placeholder="input number" name="name" onChange={e => setfName(e.target.value)} />
+        <button type="submit" onClick={submitValue}>submit</button>
+        <div>
+          {fNumber}
+        </div>
       </header>
     </div>
   );
